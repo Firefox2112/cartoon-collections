794010f661e4b13df68f43e64fdb2c7aff3b719e
Coby <cobyrpilpel2@gmail.com>
2020-12-15 21:44:23 -0500

    commit tree a59f26eb845758fe6201096c599e36ca3b23171a

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end  
+end ## OK this makes some sense. More reading on with_index needed.
+
+
+
+def summon_captain_planet(planeteer_calls)
+   planeteer_calls.map do |call|
+    call[0] = call[0].upcase
+    call = call + "!"
+   end
+   
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? do |call|
+    call.length > 4
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def find_the_cheese(strings)
+cheese_types = ["cheddar", "gouda", "camembert"]
+strings.find do |word|
+cheese_types.include?(word)
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+
+##go read the supplemental info again. Same problem. It works. Not sure why. 
